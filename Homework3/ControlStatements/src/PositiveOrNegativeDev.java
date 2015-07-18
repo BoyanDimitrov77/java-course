28482906f7d67417e7701581ba1056a4c97eaa8c
bob <bobimen@gmail.com>
2015-07-18 15:03:49 +0300

    Homework3

--- Homework3/ControlStatements/src/PositiveOrNegativeDev.java
+++ Homework3/ControlStatements/src/PositiveOrNegativeDev.java
@@ -3,20 +3,29 @@ import java.util.Scanner;
 public class PositiveOrNegativeDev {
 
 	public static void main(String[] args) {
-		Scanner input=new Scanner(System.in,"UTF-8");
-		
+		Scanner input = new Scanner(System.in, "UTF-8");
+
 		System.out.println("Enter first number:");
-		int number1=input.nextInt();
-		
+		int number1 = input.nextInt();
+
 		System.out.println("Enter second number:");
-		int number2=input.nextInt();
-		
-		if(number1>0 && number2>0){
-			System.out.println("+");
-		}else if(number1<0 && number2<0){
-			System.out.println("+");
-		}else{
-			System.out.println("-");
+		int number2 = input.nextInt();
+		if (number1 != 0 && number2 != 0) {
+
+			if (number1 > 0 && number2 > 0) {
+				System.out.println("+");
+			} else if (number1 < 0 && number2 < 0) {
+				System.out.println("+");
+			} else {
+				System.out.println("-");
+			}
+			if (number1 == 0 || number2 == 0) {
+				System.out.println("Zero");
+			}
+
+		} else {
+
+			System.out.println("Zero");
 		}
 		input.close();
 	}
